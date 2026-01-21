d77825dba1bbf8e181bff9e333241a882abd2041
Development <dev@Air-de-Jaime.versumdata.com>
2026-01-21 16:59:42 +0100

    commit inicial

---
+++ src/data/points.js
@@ -0,0 +1,67 @@
+export const puntos = [
+    {
+      nombre: "Calle Menéndez y Pelayo",
+      coords: [37.36005530045889, -6.027048336479648],
+      texto: "Referencia al Matadero en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Puerta de la Carne",
+      coords: [37.386107991499415, -5.98676944512817],
+      texto: "Mención del Matadero en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Plaza Virgen de los Reyes",
+      coords: [37.38614916878507, -5.992001487456366],
+      texto: "Corral de los Olmos en la comedia El Rufián Dichoso."
+    },
+    {
+      nombre: "Calle Santo Tomás",
+      coords: [37.38441217087605, -5.993142858620549],
+      texto: "Postigo del Alcázar en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Postigo del Carbón",
+      coords: [37.3858, -5.9932],
+      texto: "Entrada de Rinconete y Cortadillo en Sevilla."
+    },
+    {
+      nombre: "Catedral – Puerta del Perdón",
+      coords: [37.3870, -5.9928],
+      texto: "Las Gradas, lugar de contratación en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Calle Joaquín Guichot",
+      coords: [37.3891, -5.9918],
+      texto: "Antigua calle de Tintores en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Arquillo del Ayuntamiento",
+      coords: [37.3896, -5.9953],
+      texto: "Antigua Plaza de San Francisco."
+    },
+    {
+      nombre: "Plaza del Pan",
+      coords: [37.3899, -5.9940],
+      texto: "Uno de los episodios más donosos de Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Plaza de la Alcaicería",
+      coords: [37.3893, -5.9934],
+      texto: "Antigua Carnicería en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Calle Huelva",
+      coords: [37.3906, -5.9938],
+      texto: "Una de las tres cosas que el rey tenía por ganar en Sevilla."
+    },
+    {
+      nombre: "Calle Adriano",
+      coords: [37.3832, -5.9984],
+      texto: "Lugar de malbaratillo en Rinconete y Cortadillo."
+    },
+    {
+      nombre: "Calle Miguel Mañara",
+      coords: [37.3828, -5.9973],
+      texto: "Referencia al Postigo del Alcázar."
+    }
+  ];
